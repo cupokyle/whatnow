f69a0562ca4c603309be783cc45aa203f8b05c71
Kyle Forsyth <77871145+cupokyle@users.noreply.github.com>
2022-05-11 20:56:51 -0400

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -17,12 +17,16 @@ export default async function (req, res) {
 function generatePrompt(prompty) {
   const capitalizedPrompty =
     prompty[0].toUpperCase() + prompty.slice(1).toLowerCase();
-  return `Write a two sentence story about Howie.
+  return `Create a JavaScript object with three keys.
 
-Prompty: Cat
-Ideas: Howie bought a new cat. The cat attacked him and he screamed.
-Prompty: murder Jack Layton
-Ideas: Howie believes that Jack Layton was murdered. He's always talking about it.
+Prompty: You walk to the bank. 
+Ideas:{"context": "You walk to the bank. The bank is a bustling building. Many people are entering and exiting with wads of cash in their hands.",
+"leftButton": "Go inside the bank.", 
+"rightButton": "Throw a rock at the window."}
+Prompty: A customer introduces themself to you.
+Ideas:{"context": "A customer introduces themself to you. The customer has long blonde hair and a beautiful smile.",
+"leftButton": "Politely say Hello", 
+"rightButton": "Scoff and walk away"}
 Prompty: ${capitalizedPrompty}
 Ideas:`;
 }
