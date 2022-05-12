74bd408331cba8143b155d0d9ab0c0c2a9314a67
Kyle Forsyth <77871145+cupokyle@users.noreply.github.com>
2022-05-11 20:54:43 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,13 @@ import styles from "./index.module.css";
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
-  const [result, setResult] = useState();
+  const [result, setResult] = useState("");
+  const [leftButton, setLeftButton] = useState("");
+  const [rightButton, setRightButton] = useState("");
 
   async function onSubmit(event) {
     event.preventDefault();
+    console.log("userInput", userInput);
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
@@ -16,32 +19,72 @@ export default function Home() {
       body: JSON.stringify({ prompty: userInput }),
     });
     const data = await response.json();
-    setResult(data.result);
+    const newData = JSON.parse(data.result);
+    setResult(newData.context);
+    setLeftButton(newData.leftButton);
+    setRightButton(newData.rightButton);
+    setUserInput("");
+  }
+  async function onSubmitButton(event) {
+    event.preventDefault();
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompty: "You " + userInput }),
+    });
+    const data = await response.json();
+    const newData = JSON.parse(data.result);
+    setResult(newData.context);
+    setLeftButton(newData.leftButton);
+    setRightButton(newData.rightButton);
     setUserInput("");
   }
 
   return (
     <div>
       <Head>
-        <title>HowieGen</title>
-        <link rel="icon" href="/kyle.jpg" />
+        <title>What Now?</title>
+        <link rel="icon" href="/badlibs.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/kyle.jpg" className={styles.icon} />
-        <h3>HowieGen</h3>
-        <h3>Is down for maintenance</h3>
-        {/* <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="prompty"
-            placeholder="Enter a prompt"
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-          />
-          <input type="submit" value="Generate Text" />
-        </form> */}
+        <img src="/badlibs.png" className={styles.icon} />
+        <h3>What Now?</h3>
+        <form onSubmit={onSubmit}>
+          {!result && (
+            <input
+              type="text"
+              name="prompty"
+              placeholder="Enter a prompt"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+            />
+          )}
+          {!result && <input type="submit" value="Generate Text" />}
+        </form>
         <div className={styles.result}>{result}</div>
+        <form onSubmit={onSubmitButton} className={styles.buttonbox}>
+          {result && (
+            <input
+              className={styles.button}
+              type="submit"
+              name="prompty"
+              value={leftButton}
+              onClick={(e) => setUserInput(e.target.value)}
+            />
+          )}
+          {result && (
+            <input
+              className={styles.button}
+              type="submit"
+              name="prompty"
+              value={rightButton}
+              onClick={(e) => setUserInput(e.target.value)}
+            />
+          )}
+        </form>
       </main>
     </div>
   );
