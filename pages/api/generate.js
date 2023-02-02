ce78459236eb5636cb58d5b25670b87440ffde1c
cupokyle <kyleforsythcomedy@gmail.com>
2023-02-01 23:46:02 -0500

    add image gen and newer text model and 4 choice

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,15 +1,16 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion("text-davinci-002", {
-    prompt: generatePrompt(req.body.prompty),
-    temperature: 0.75,
-    max_tokens: 64,
+  const ultimatePrompt = generatePrompt(req.body.prompty);
+  const completion = await openai.createCompletion("text-davinci-003", {
+    prompt: ultimatePrompt,
+    temperature: 0.9,
+    max_tokens: 122,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
@@ -21,12 +22,16 @@ function generatePrompt(prompty) {
 
 Prompty: You walk to the bank. 
 Ideas:{"context": "You walk to the bank. The bank is a bustling building. Many people are entering and exiting with wads of cash in their hands.",
-"leftButton": "Go inside the bank.", 
-"rightButton": "Throw a rock at the window."}
+"leftButton": "Go inside the bank to open a chequing account",
+"rightButton": "Think about banks quizically",
+"leftTwoButton": "Scream the word Bank until you can't breathe anymore",
+"rightTwoButton": "Throw a rock at the window."}
 Prompty: A customer introduces themself to you.
 Ideas:{"context": "A customer introduces themself to you. The customer has long blonde hair and a beautiful smile.",
-"leftButton": "Politely say Hello", 
-"rightButton": "Scoff and walk away"}
+"leftButton": "Politely say Hello",
+"rightButton": "Ask them if they have any games on their phone", 
+"leftTwoButton": "Comment on their beautful hair",  
+"rightTwoButton": "Ask them if there's any way you can help them today"}
 Prompty: ${capitalizedPrompty}
 Ideas:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
+import axios from 'axios';
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
   const [result, setResult] = useState("");
   const [leftButton, setLeftButton] = useState("");
   const [rightButton, setRightButton] = useState("");
+  const [leftTwoButton, setLeftTwoButton] = useState("");
+  const [rightTwoButton, setRightTwoButton] = useState("");
+  const [imgResult, setImgResult] = useState("");
 
   async function onSubmit(event) {
+    setImgResult('https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e471pwmo03hyhg4v6pv4fg8a0pcfrucr5ob7oudbhgw&rid=giphy.gif&ct=g');
     event.preventDefault();
     const response = await fetch("/api/generate", {
       method: "POST",
@@ -17,15 +22,43 @@ export default function Home() {
       },
       body: JSON.stringify({ prompty: userInput }),
     });
+
+    var imgdata = JSON.stringify({
+      "prompt": userInput + ", digital art",
+      "n": 1,
+      "size": "1024x1024"
+    });
+
+    var config = {
+      method: 'post',
+      url: 'https://api.openai.com/v1/images/generations',
+      headers: {
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      data: imgdata
+    };
+
+    axios(config)
+      .then(function (res) {
+        setImgResult(res.data.data[0].url);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
     const data = await response.json();
     const newData = JSON.parse(data.result);
     setResult(newData.context);
     setLeftButton(newData.leftButton);
     setRightButton(newData.rightButton);
+    setLeftTwoButton(newData.leftTwoButton);
+    setRightTwoButton(newData.rightTwoButton);
     setUserInput("");
   }
   async function onSubmitButton(event) {
     event.preventDefault();
+    setImgResult('https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e471pwmo03hyhg4v6pv4fg8a0pcfrucr5ob7oudbhgw&rid=giphy.gif&ct=g');
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
@@ -35,10 +68,36 @@ export default function Home() {
     });
     const data = await response.json();
     const newData = JSON.parse(data.result);
-    console.log(newData);
+
+    var imgdata = JSON.stringify({
+      "prompt": "You " + userInput + ", digital art",
+      "n": 1,
+      "size": "1024x1024"
+    });
+
+    var config = {
+      method: 'post',
+      url: 'https://api.openai.com/v1/images/generations',
+      headers: {
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      data: imgdata
+    };
+
+    axios(config)
+      .then(function (res) {
+        setImgResult(res.data.data[0].url);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
     setResult(newData.context);
     setLeftButton(newData.leftButton);
     setRightButton(newData.rightButton);
+    setLeftTwoButton(newData.leftTwoButton);
+    setRightTwoButton(newData.rightTwoButton);
     setUserInput("");
   }
 
@@ -53,6 +112,7 @@ export default function Home() {
         <img src="/badlibs.png" className={styles.icon} />
         <h3>What Now?</h3>
         <form onSubmit={onSubmit}>
+          {imgResult === 'https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e471pwmo03hyhg4v6pv4fg8a0pcfrucr5ob7oudbhgw&rid=giphy.gif&ct=g' && <img src={imgResult} width="100px" />}
           {!result && (
             <input
               type="text"
@@ -64,10 +124,11 @@ export default function Home() {
           )}
           {!result && <input type="submit" value="Generate Text" />}
         </form>
+        {imgResult !== '' && imgResult !== 'https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e471pwmo03hyhg4v6pv4fg8a0pcfrucr5ob7oudbhgw&rid=giphy.gif&ct=g' && <img src={imgResult} width="200px" />}
         <div className={styles.result}>{result}</div>
         <form onSubmit={onSubmitButton} className={styles.buttonbox}>
-          {result && !leftButton && !rightButton && <h3>Game Over</h3>}
-          {result && leftButton && (
+          {result && !leftButton && !rightButton && !leftTwoButton && !rightTwoButton && <h3>Game Over</h3>}
+          {imgResult !== 'https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e471pwmo03hyhg4v6pv4fg8a0pcfrucr5ob7oudbhgw&rid=giphy.gif&ct=g' && result && leftButton && (
             <input
               className={styles.button}
               type="submit"
@@ -76,12 +137,30 @@ export default function Home() {
               onClick={(e) => setUserInput(e.target.value)}
             />
           )}
-          {result && rightButton && (
+          {imgResult !== 'https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e471pwmo03hyhg4v6pv4fg8a0pcfrucr5ob7oudbhgw&rid=giphy.gif&ct=g' && result && rightButton && (
             <input
               className={styles.button}
               type="submit"
               name="prompty"
               value={rightButton}
+              onClick={(e) => setUserInput(e.target.value)}
+            />
+          )}
+          {imgResult !== 'https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e471pwmo03hyhg4v6pv4fg8a0pcfrucr5ob7oudbhgw&rid=giphy.gif&ct=g' && result && leftTwoButton && (
+            <input
+              className={styles.button}
+              type="submit"
+              name="prompty"
+              value={leftTwoButton}
+              onClick={(e) => setUserInput(e.target.value)}
+            />
+          )}
+          {imgResult !== 'https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e471pwmo03hyhg4v6pv4fg8a0pcfrucr5ob7oudbhgw&rid=giphy.gif&ct=g' && result && rightTwoButton && (
+            <input
+              className={styles.button}
+              type="submit"
+              name="prompty"
+              value={rightTwoButton}
               onClick={(e) => setUserInput(e.target.value)}
             />
           )}
